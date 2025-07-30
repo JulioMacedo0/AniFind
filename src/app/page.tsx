ebf84eadb23a3ca653f437b24098f93d25fb1bbf
Julio Macedo <juliocesar1076@gmail.com>
2025-07-30 11:55:33 -0300

    feat: remove props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,7 @@ export default function Home() {
     <div className="font-sans min-h-screen ">
       <main className="flex flex-col gap-[32px] items-center">
         <div className="w-full max-w-4xl">
-          <FileUpload
-            maxFiles={3}
-            maxFileSize={1 * 1024 * 1024} // 5MB
-          />
+          <FileUpload />
         </div>
       </main>
     </div>
