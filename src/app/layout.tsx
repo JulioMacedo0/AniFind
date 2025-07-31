32b7cdb8e4c13ffcc2d280e181479c161801d091
Julio Macedo <juliocesar1076@gmail.com>
2025-07-31 16:06:21 -0300

    feat: add umami analittics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          src="https://umami.juliomacedo.dev/script.js"
+          data-website-id="065d2b3d-98ac-46ac-b510-ac52b8fba09d"
+          strategy="afterInteractive"
+        />
         {children}
         <Toaster />
       </body>
