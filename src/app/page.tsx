16285fa878608ccef4bb30557a3c18f55fc63dd5
Julio Macedo <juliocesar1076@gmail.com>
2025-08-03 23:08:29 -0300

    feat: add anime apresentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { UploadSection } from "@/components/UploadSection";
 import { AnimeResults } from "@/components/AnimeResults";
+import { AnimeApresentation } from "@/components/AnimeApresentation";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-slate-900 via-slate-800 to-black">
       <main>
         <UploadSection />
-        <AnimeResults />
+        <AnimeApresentation />
       </main>
     </div>
   );
