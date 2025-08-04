bd0fbff6fd6fb9af85b33301568005da1c3f9478
Julio Macedo <juliocesar1076@gmail.com>
2025-08-04 02:24:09 -0300

    feat: use local docker ip on redis

---
+++ src/lib/redis.ts
@@ -0,0 +1,8 @@
+// redisClient.ts
+import { createClient } from "redis";
+
+export const redis = createClient({
+  url: process.env.REDIS_URL || "redis://172.17.0.1:6379",
+  password: process.env.REDIS_PASSWORD || "dev123456",
+});
+redis.connect();
