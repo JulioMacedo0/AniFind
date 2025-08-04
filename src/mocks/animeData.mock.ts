9a75f2d8f014cd5f1d9b2547159968e0ea993e4e
Julio Macedo <juliocesar1076@gmail.com>
2025-08-04 09:59:27 -0300

    feat: create mock

---
+++ src/mocks/animeData.mock.ts
@@ -0,0 +1,21 @@
+import { AnimeData } from "@/@types";
+
+export const sampleAnime: AnimeData = {
+  name: "Attack on Titan",
+  description:
+    "Humanity fights for survival against giant humanoid Titans that have brought civilization to the brink of extinction. When the Titans breach Wall Maria, Eren Yeager vows to eliminate every last Titan and reclaim the world for humanity.",
+  coverImage: "https://m.media-amazon.com/images/I/71uHZO5ChkL._AC_SY879_.jpg",
+  video:
+    "https://cdn.juliomacedo.dev/previews/Solo_Leveling/Solo_Leveling_S01E01_903.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=admin%2F20250804%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250804T024308Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=d43a144a02f491ca10760b4c7c3a6c872e2ed4d6a61a6a23649eb1b9c01ca459",
+  rating: 9.0,
+  year: 2013,
+  episodes: 87,
+  genre: ["Action", "Drama", "Fantasy"],
+  studio: "Mappa",
+  status: "Completed",
+  matchPercentage: 14.1,
+  season: 1,
+  episode: 5,
+  timeCode: "00:15:03",
+  streamingUrl: "https://www.crunchyroll.com/attack-on-titan",
+};
