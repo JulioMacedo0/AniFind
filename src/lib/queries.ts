820d5c33eb8a60cf097e1a358af618d18ecbdf37
Julio Macedo <juliocesar1076@gmail.com>
2025-08-04 01:15:52 -0300

    feat: create graphql querry

---
+++ src/lib/queries.ts
@@ -0,0 +1,32 @@
+// lib/queries.ts
+import { gql } from "@apollo/client";
+
+export const GET_ANIME_BY_ID = gql`
+  query ($id: Int) {
+    Media(id: $id, type: ANIME) {
+      title {
+        romaji
+      }
+      description
+      coverImage {
+        extraLarge
+      }
+      trailer {
+        site
+        id
+      }
+      averageScore
+      startDate {
+        year
+      }
+      episodes
+      genres
+      studios(isMain: true) {
+        nodes {
+          name
+        }
+      }
+      status
+    }
+  }
+`;
