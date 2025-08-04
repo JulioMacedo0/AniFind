2b2661d3088b5fe871aed9b1471d7e1a47791978
Julio Macedo <juliocesar1076@gmail.com>
2025-08-04 09:09:25 -0300

    feat: handle connect and reconection redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,18 @@
-// redisClient.ts
+// lib/redis.ts
 import { createClient } from "redis";
 
 export const redis = createClient({
   url: process.env.REDIS_URL || "redis://172.17.0.1:6379",
 });
-redis.connect();
+
+redis.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
+
+redis.on("connect", () => {
+  console.log("Redis connected successfully");
+});
+
+if (!redis.isOpen) {
+  redis.connect().catch(console.error);
+}
