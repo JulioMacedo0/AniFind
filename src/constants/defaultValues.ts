328486ce24b8d55c6f386af96810475dc7ce3bec
Julio Macedo <juliocesar1076@gmail.com>
2025-08-09 21:17:13 -0300

    feat: update validation image

--- src/constants/defaultValues.ts
+++ src/constants/defaultValues.ts
@@ -1,4 +1,6 @@
 export const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2MB default
 export const ACCEPTED_MIME_TYPES = {
-  "image/*": [".png", ".jpg", ".jpeg", ".webp"],
+  "image/png": [".png"],
+  "image/jpeg": [".jpg", ".jpeg"],
+  "image/webp": [".webp"],
 };
